d58a82d24ca0fd32695304d178996d1853ad82ea
farishuric <74626441+farishuric@users.noreply.github.com>
2022-10-10 12:12:08 +0200

    Initial Commit

---
+++ BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Faris Hurić on 10. 10. 2022..
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BottomSheet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BottomSheet
+//
+//  Created by Faris Hurić on 10. 10. 2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
